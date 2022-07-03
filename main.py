7e009ad9bac4fd66fbe672938473d7cebec15cc6
JDSNX <judisonbacalso24@gmail.com>
2022-07-03 19:02:33 +0800

    added mp detection and landmark functions

--- main.py
+++ main.py
@@ -6,21 +6,26 @@ mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0)
 
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+def draw_landmarks(image, results):
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
+
         ret, frame = cap.read()
 
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        results = holistic.process(image)
+        res_image, results = mediapipe_detection(frame, holistic)
         
-        res_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Right Hand Landmarks
-        mp_drawing.draw_landmarks(res_image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-        
-        # Left Hand Landmarks
-        mp_drawing.draw_landmarks(res_image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        draw_landmarks(res_image, results)
 
         cv2.imshow('Hollistic Model Detection', res_image)
 
